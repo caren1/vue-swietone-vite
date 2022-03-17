58b0f697947b86a611497c8761a29101c7d9786f
caren1 <30754368+caren1@users.noreply.github.com>
2022-03-17 22:08:53 +0100

    meh..

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -32,7 +32,7 @@ export default {
     cta.addEventListener("click", clickHandler);
   
  
-function clickHandler(e) {
+  function clickHandler(e) {
   e.preventDefault();
   const href = this.getAttribute("href");
   const offsetTop = document.querySelector(href).offsetTop;
